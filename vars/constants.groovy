bec2e8501e3b90c14ebaff97182e7bc900a50ad6
Yashwanth4147 <102645471+Yashwanth4147@users.noreply.github.com>
2025-02-04 15:09:08 +0530

    Update constants.groovy
    added the details in script

--- vars/constants.groovy
+++ vars/constants.groovy
@@ -1,10 +1,10 @@
 // vars/constants.groovy
 import groovy.transform.Field
 
-@Field String AWS_ACCOUNT_ID = '207567760307'
-@Field String AWS_DEFAULT_REGION = 'us-east-1'
+@Field String AWS_ACCOUNT_ID = '820242919738'
+@Field String AWS_DEFAULT_REGION = 'ap-south-1'
 @Field String IMAGE_REPO_NAME = 'sample-webapp'
-@Field String GIT_REPO_URL = 'https://github.com/girishballur123/Maven-project.git'
+@Field String GIT_REPO_URL = 'https://github.com/Yashwanth4147/Docker-project-sample.git'
 
 // Jenkins Credential IDs
 @Field String GIT_CREDENTIALS_ID = 'Jenkinslatest'
